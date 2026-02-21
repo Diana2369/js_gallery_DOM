5d551fd1b4145d45fd16c8126de40114523bedd1
Diana <dinamitka2369@gmail.com>
2026-02-21 17:43:40 +0100

    result1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const thumbsList = document.getElementById('thumbs');
+const largeImg = document.getElementById('largeImg');
+
+thumbsList.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  let target = event.target;
+
+  if (target.tagName.toLowerCase() === 'img') {
+    target = target.closest('a');
+  }
+
+  if (target.tagName.toLowerCase() === 'a') {
+    const newSrc = target.getAttribute('href');
+    largeImg.setAttribute('src', newSrc);
+  }
+});
